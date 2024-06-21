cbe1b402245d3489d1b84f3fd121b68d0885dc88
opzede <67693411+opzede@users.noreply.github.com>
2024-06-22 00:00:38 +0800

    Update use_resender.py

--- use_resender.py
+++ use_resender.py
@@ -2,7 +2,9 @@ from okx.api import API
 from okx.app.utils import eprint
 
 if __name__ == '__main__':
+    # test
     proxy_host = 'http://43.156.53.213/'  # OKX_resender地址
     api = API(proxy_host=proxy_host)
     ticker_result = api.market.get_ticker(instId='BTC-USDT')
     eprint(ticker_result, length=20)
+    
